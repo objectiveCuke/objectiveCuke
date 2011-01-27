a46178fee3551476f3c9709df6b1690c9b111bc8
Grant McInnes <grant.mcinnes@ehealthinnovation.org>
2011-01-27 13:23:34 -0500

    Add OBCRequestHandler to respond to incoming requests

---
+++ objectiveCuke/OBCRequestHandler.h
@@ -0,0 +1,16 @@
+//
+//  OBCRequestHandler.h
+//  objectiveCuke
+//
+//  Created by Grant McInnes on 11-01-27.
+//  Copyright 2011 eyesopen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface OBCRequestHandler : NSObject {
+
+}
+
+@end
---
+++ objectiveCuke/OBCRequestHandler.m
@@ -0,0 +1,14 @@
+//
+//  OBCRequestHandler.m
+//  objectiveCuke
+//
+//  Created by Grant McInnes on 11-01-27.
+//  Copyright 2011 eyesopen. All rights reserved.
+//
+
+#import "OBCRequestHandler.h"
+
+
+@implementation OBCRequestHandler
+
+@end
